eb1cc58e6f73777aa7278f639d765c7ecd3e8ad8
qxiaoguiq195 <qxiaoguiq@gmail.com>
2026-09-22 12:10:06 +0800

    Update keygen.cpp

--- keygen.cpp
+++ keygen.cpp
@@ -4,7 +4,7 @@
 #pragma comment(lib, "version.lib")
 
 namespace {
-    constexpr char ALPHABET[] = "DY14UF3RHWCXLQB6IKJT9N5AGS2PM8VZ7";
+    constexpr char ALPHABET[] = "48M41-D38D6-UZDJT-11E8Y-98YMT";
 
     bool base34_encode(uint32_t value, size_t width, std::string& out) {
         uint64_t limit = 1;
